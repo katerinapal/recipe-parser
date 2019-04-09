36e1370134c8e33f81d20d27defff6c07ca3bfe0
katerina <katpaltoglou@gmail.com>
2019-04-09 10:17:42 +0300

    update transpiled branch

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-import recipeparser from "./lib/recipe-parser";
-export default recipeparser;
+var _recipeParser = require("./lib/recipe-parser");
+
+var _recipeParser2 = _interopRequireDefault(_recipeParser);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _recipeParser2.default;
+module.exports = exports.default;
