9c55911e5d2db2b4e3bbb034968f462f1c8a9d03
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-03-26 19:44:16 +0200

    update branch

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 
-module.exports = require('./lib/recipe-parser');
+import { RecipeParser as librecipeparser_RecipeParserjs } from "./lib/recipe-parser";
+var indexjs_indexjs = librecipeparser_RecipeParserjs;
+export { indexjs_indexjs as indexjs };
+export { indexjs_indexjs as indexjs };
